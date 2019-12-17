a4b00aa9c119e7ee61f03cfd87d773a6e943ef10
Evan Czyzycki <eczy3826@gmail.com>
2019-12-17 14:45:56 -0800

    Merge dev

--- make_tabular_datasets.py
+++ make_tabular_datasets.py
@@ -4,6 +4,7 @@ from glob import glob
 from sklearn.model_selection import train_test_split
 import regex
 import csv
+from collections import defaultdict
 
 TOKENS = regex.compile(r"\p{L}+|[.?!]")
 
@@ -17,29 +18,54 @@ def tokenize(text):
 # def create_dataset(dataset_source):
 def create_dataset():
     examples = []
+    examples_by_source = defaultdict(list)
     with open("unified-dataset.jsonl") as f:
         for line in f:
             datum = json.loads(line)
-            # source = datum["source"]
+            source = datum["source"]
             # if datum["source"] != dataset_source:
             # continue
             # examples.append((map_emotion(datum), tokenize(datum["text"]), source))
-            examples.append((map_emotion(datum), tokenize(datum["text"])))
-        train, test = train_test_split(examples)
+            emotion = get_emotion(datum)
+            text = tokenize(datum["text"])
+            examples.append((emotion, text))
+            examples_by_source[source].append((emotion, text))
+        # train, test = train_test_split(examples)
     # with open(f"{dataset_source}.csv", "w") as f:
     with open("unified_without_source.csv", "w") as csvfile:
-        fieldnames = ["label", "text", "is_valid"]
+        fieldnames = ["label", "text"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for number, text in test:
+        for number, text in examples:
             if number is None or not text:
                 continue
-            writer.writerow({"label": number, "text": text, "is_valid": True})
-        for number, text in train:
-            if number is None or not text:
-                continue
-            writer.writerow({"label": number, "text": text, "is_valid": True})
+            writer.writerow({"label": number, "text": text})
+        # for number, text in test:
+        #     if number is None or not text:
+        #         continue
+        #     writer.writerow({"label": number, "text": text})
+        # for number, text in train:
+        #     if number is None or not text:
+        #         continue
+        #     writer.writerow({"label": number, "text": text})
 
+    for source, examples in examples_by_source.items():
+        with open(f"{source}.csv", "w") as csvfile:
+            fieldnames = ["label", "text"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for number, text in examples:
+                if number is None or not text:
+                    continue
+                writer.writerow({"label": number, "text": text})
+            # for number, text in test:
+            #     if number is None or not text:
+            #         continue
+            #     writer.writerow({"label": number, "text": text})
+            # for number, text in train:
+            #     if number is None or not text:
+            #         continue
+            #     writer.writerow({"label": number, "text": text})
 
 # os.makedirs()
 
@@ -62,9 +88,8 @@ emo2id = {
     "guilt": None,
 }
 
-
-def map_emotion(datum):
-    emo_val = [
+def emotion_val(datum):
+    return [
         (
             (
                 datum["emotions"][emo]
@@ -75,10 +100,20 @@ def map_emotion(datum):
         )
         for emo in datum["emotions"]
     ]
+
+def map_emotion(datum):
+    emo_val = emotion_val(datum)
     if sum(x[0] for x in emo_val) > 0:
         return emo2id[max(emo_val)[1]]
     else:
         return emo2id["noemo"]
+
+def get_emotion(datum):
+    emo_val = emotion_val(datum)
+    if sum(x[0] for x in emo_val) > 0:
+        return max(emo_val)[1]
+    else:
+        return "noemo"
 
 
 if __name__ == "__main__":
